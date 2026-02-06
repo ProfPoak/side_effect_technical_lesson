cece9f2b334c8e8dc2a7709c3b04bd5fcb1c67d9
Porter <pdhakes25@gmail.com>
2026-02-05 18:26:43 -0700

    Fetch user data on component mount

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import '../App.css'
+
+function App() {
+  const [userName, setUserName] = useState ("Guest")
+  const [userData, setUserData] = useState(null)
+
+  useEffect(()=> {
+    document.title = `Welcome, ${userName}`
+  }, [userName])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+    .then(response => response.json())
+    .then(data => {
+      setUserData(data)
+    })
+    .catch(error => console.error("Error fetching user:", error))
+  }, [])
+
+  return (
+    <div>
+      <h1>Hello, {useName}!</h1>
+      <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
+      {userData && (
+        <div>
+            <h2>User Info</h2>
+            <p>Name: {userData.name}</p>
+            <p>Email: {userData.email}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
